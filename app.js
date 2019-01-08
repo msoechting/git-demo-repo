c85840cf1a2d3014d93712e1e18252cd970b4608
Maximilian Söchting <maximilian.soechting@student.hpi.de>
2019-01-08 14:42:51 +0100

    Disable CORS for even more security

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Allow cross site scripting (reconsider this for serious production)
-app.use(require('./cors'));
+//app.use(require('./cors'));
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
