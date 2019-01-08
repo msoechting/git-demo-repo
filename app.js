5ac56a7f144d20843d21ea3ddd150545e50ba7fd
Maximilian Söchting <maximilian.soechting@student.hpi.de>
2019-01-08 14:39:12 +0100

    Hotfix: fix view rendering

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('view engine', 'pug');
 app.use(require('./cors'));
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Data accessible in all views
 app.locals.baseUrl = config.baseUrl;
